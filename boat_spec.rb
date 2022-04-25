f24ed170acde63bf253d2d804e4677b6c18ea676
PaolaP <paolaperez8@gmail.com>
2022-04-24 21:42:57 -0400

    test assert fail

--- boat_spec.rb
+++ boat_spec.rb
@@ -2,11 +2,11 @@ require_relative 'boat'
 
 describe Boat do
   # every it is a test, the string is a description of the test. in this case that boat class can create instances
-  require_relative 'boat'
+  
+# assert if return value is a different class like a hash
 
-describe Boat do
   it 'should create boats' do
     expect(Boat.new).to be_a Hash
   end
-end
+
 end
