a67903e6a07eaa14706f589043a5ab7ce456083b
PaolaP <paolaperez8@gmail.com>
2022-04-24 21:40:37 -0400

    edit test to fail

--- boat_spec.rb
+++ boat_spec.rb
@@ -2,7 +2,11 @@ require_relative 'boat'
 
 describe Boat do
   # every it is a test, the string is a description of the test. in this case that boat class can create instances
+  require_relative 'boat'
+
+describe Boat do
   it 'should create boats' do
-    expect(Boat.new).to be_a Boat
+    expect(Boat.new).to be_a Hash
   end
+end
 end
