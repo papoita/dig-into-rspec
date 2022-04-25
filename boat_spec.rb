e09dbd71c59cfe19b8c56fd5be70491abd5fd66e
PaolaP <paolaperez8@gmail.com>
2022-04-24 21:48:32 -0400

    add example nested describe to test

--- boat_spec.rb
+++ boat_spec.rb
@@ -1,13 +1,9 @@
 require_relative 'boat'
 
 describe Boat do
-  # every it is a test, the string is a description of the test. in this case that boat class can create instances
-  
-# assert if return value is a different class like a hash
-
-
   it 'should create boats' do
     expect(Boat.new).to be_a Boat
   end
-
+  describe '#allowed_aboard?' do
+  end
 end
